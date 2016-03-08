3ce4632f3592569e983af453c8da39c92c1ea2a6
Connor Fitzpatrick <fitzco01@connors-macbook-air.local>
2016-03-07 22:52:55 -0600

    Initial Commit

---
+++ Psychologist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Psychologist
+//
+//  Created by Connor Fitzpatrick on 3/7/16.
+//  Copyright © 2016 Connor Fitzpatrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
